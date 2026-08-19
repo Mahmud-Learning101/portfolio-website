8d4d712c5fa6cf42a026cefb6c44571d9d2e9f9b
Mahmud-Learning101 <Mahmud-Learning101@users.noreply.github.com>
2026-08-19 17:08:09 +0600

    feat: enable dynamic real-time data fetching for admin updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const revalidate = 0;
+
 export const metadata: Metadata = {
   title: "S. M. Mahmud Bin Murad | Marketing & Content Strategist | Project Manager",
   description:
